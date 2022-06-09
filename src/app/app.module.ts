0cd231acd7eb18bd569620c907ea07be8a5257d3
LewBlu <lewis.blundell.development@gmail.com>
2022-06-09 22:04:14 +0100

    Moved exercise list items to its own component
    (tidy up of app module component list)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,25 @@ import { NgChartsModule } from 'ng2-charts';
 import { environment } from '../environments/environment';
 import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ExerciseComponent } from './log-book/exercise/exercise.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { FooterComponent } from './navigation/footer/footer.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { LogBookComponent } from './log-book/log-book.component';
-import { Error404Component } from './error404/error404.component';
+import { ExerciseComponent } from './log-book/exercise/exercise.component';
 import { ChartComponent } from './progress/chart/chart.component';
 import { ProgressComponent } from './progress/progress.component';
 import { SettingsComponent } from './settings/settings.component';
 import { ExercisesComponent } from './settings/exercises/exercises.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { ExerciseItemComponent } from './settings/exercises/exercise-item/exercise-item.component';
 import { WeightFormComponent } from './progress/weight-form/weight-form.component';
+
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
